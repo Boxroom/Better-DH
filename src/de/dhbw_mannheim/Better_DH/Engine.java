23303ccc386461615b2295ebefb37ecafbb565a6
Louisa <Louisa@Louisa-PC.Speedport_W723_V_Typ_A_1_01_011>
2015-11-09 09:36:04 +0100

    Die Berechnung der einzelnen Parameter

--- src/de/dhbw_mannheim/Better_DH/Engine.java
+++ src/de/dhbw_mannheim/Better_DH/Engine.java
@@ -29,6 +29,16 @@ import org.w3c.dom.Attr;
  */
 public class Engine {
 	
+	final static double mealMAX = 5,
+			stockMAX = 8000,
+			eventsMAX = 7,
+			amountLecturerMAX = 500,
+			capitalMAX = 10000000, 
+			adMAX = 6000,
+			salaryLecturerMAX = 10000,
+			amountCompaniesMAX = 4000,
+			amountStudentsMAX = 10000;
+	
 	public static void main(String[] args){
 
 		           
@@ -308,6 +318,91 @@ public class Engine {
 		public void loadPlayer(String name){
 			Import(name);
 			
+		}
+		
+		public void simulate(){
+			//TODO
+			double averageMeal = 3/mealMAX;
+			double averageStock = 4000/stockMAX;
+			double averageEvents = 2/eventsMAX;
+			double averageLecturer = 50/amountLecturerMAX;
+			double averageCapital = 100000/capitalMAX;
+			double averageAd = 6000/adMAX;
+			double averageSalaryLecturer = 10000/salaryLecturerMAX;
+			double spots = 10000;
+			
+			double qualityDH = qualityDH(averageStock, averageEvents, averageMeal, averageLecturer, averageCapital);
+			double reputationDH = reputationDH(averageStock, averageEvents, averageAd, averageLecturer, qualityDH);
+			double satisfactionStudents = satisfactionStudents(averageStock, averageEvents, averageMeal, averageLecturer, qualityDH, reputationDH);
+			double satisfactionLecturer = satisfactionLecturer(averageStock, averageEvents, averageMeal, averageSalaryLecturer, qualityDH, reputationDH);
+			int amountCompanies = amountCompanies(qualityDH, averageStock, reputationDH, averageEvents, averageAd, satisfactionStudents);
+			int amountStudents = amountStudents(satisfactionStudents, qualityDH, averageStock, reputationDH, amountCompanies, averageAd, averageEvents, averageMeal, spots);
+
+		}
+		//Die einzelnen Parameter werden nacheinander berechnet.
+		//Zuerst mit Hilfe einer gewichteten Durchschnittsberechnung.
+		//Dieser Prozentwert wird mit einer Sinusfunktion an die Realität angenähert
+		public static double qualityDH(double stock, double events, double meal, double amountLecturer, double capital){
+			double qualityDH = 0;
+			
+			qualityDH = (3*stock+2*events+meal+amountLecturer+2*capital)/9;
+			
+			qualityDH = Math.sin((Math.PI/2)*qualityDH);
+			
+			return Math.round(qualityDH * 100)/100.00;
+		}
+		
+		public static double reputationDH(double stock, double events, double ad, double amountLecturer, double qualityDH){
+			double reputationDH = 0;
+			
+			reputationDH = (2*stock+3*events+2*ad+amountLecturer+2*qualityDH)/10;
+			
+			reputationDH = Math.sin((Math.PI/2)*reputationDH);
+			
+			return Math.round(reputationDH * 100)/100.00;
+		}
+		
+		public static double satisfactionStudents(double stock, double events, double meal, double amountLecturer, double qualityDH, double reputationDH){
+			double satisfactionStudents = 0;
+			
+			satisfactionStudents = (3*stock+events+2*meal+2*amountLecturer+3*qualityDH+2*reputationDH)/13;
+			
+			satisfactionStudents = Math.sin((Math.PI/2)*satisfactionStudents);
+			
+			return Math.round(satisfactionStudents * 100)/100.00;
+		}
+
+		public static double satisfactionLecturer(double stock, double events, double meal, double salaryLecturer, double qualityDH, double reputationDH){
+			double satisfactionLecturer = 0;
+			
+			satisfactionLecturer = (2*stock+2*events+meal+3*salaryLecturer+2*qualityDH+2*reputationDH)/12;
+			
+			satisfactionLecturer = Math.sin((Math.PI/2)*satisfactionLecturer);
+			
+			return Math.round(satisfactionLecturer * 100)/100.00;
+		}
+
+		public static int amountCompanies(double qualityDH, double stock, double reputationDH, double events, double ad, double satisfactionStudents){
+			double amountCompanies = 0;
+			
+			amountCompanies = (3*qualityDH+stock+2*reputationDH+events+2*ad+satisfactionStudents)/10;
+			amountCompanies = Math.sin((Math.PI/2)*amountCompanies);
+			return (int) (amountCompanies * amountCompaniesMAX);
+			
+		}
+		
+		public static int amountStudents(double satisfactionStudents, double qualityDH, double stock, double reputationDH, int amountCompanies, double ad, double events, double meal, double spots){
+			double prozentStudenten = 0;
+			int anzahlStudenten = 0;
+			
+			prozentStudenten = (3*satisfactionStudents+qualityDH+2*stock+2*reputationDH+2*amountCompanies+ad+events+meal)/13;
+			prozentStudenten = Math.sin((Math.PI/2)*prozentStudenten);
+			anzahlStudenten = (int) (prozentStudenten * amountStudentsMAX);
+			if(anzahlStudenten <= spots){
+				return anzahlStudenten;
+			}else{
+				return (int) spots;
+			}
 		}
 
 	
